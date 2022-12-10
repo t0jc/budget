2c4fb03c65404e1ae1e6db9aa992e668f98fbdba
JerryChang <jerrycn3675@gmail.com>
2022-12-10 17:06:08 +0800

    upd

--- src/service/service.go
+++ src/service/service.go
@@ -1,3 +1,4 @@
+// It returns the budget of a given time period.
 package service
 
 import (
@@ -13,7 +14,7 @@ type BucketService struct {
 	repo repo.IBudgetRepo
 }
 
-const YM = "200602"
+const YM = "200601"
 
 // Query implements IBucketService
 func (b *BucketService) Query(start, end time.Time) (budget int64) {
@@ -22,8 +23,16 @@ func (b *BucketService) Query(start, end time.Time) (budget int64) {
 	}
 	var lst []repo.BudgetData
 	for _, v := range b.repo.GetAll() {
-		
+		_ym, _ := time.Parse(YM, string(v.YearMonth))
+		if inMonth(_ym, start, end) {
+			lst = append(lst, v)
+		}
 	}
+
+	// for _, v := range lst {
+
+	// }
+
 	return 0
 }
 
@@ -32,7 +41,27 @@ func MonthDays(ym string) int {
 	if err != nil {
 		panic(err)
 	}
-	return time.Date(t.Year(), t.Month()+1, 0, 0, 0, 0, 0, time.Local)..Day()
+	return time.Date(t.Year(), t.Month()+1, 0, 0, 0, 0, 0, time.Local).Day()
+}
+
+func GetAllDays(start, end time.Time) (days []string) {
+	for t := start; t.After(start) && t.Before(end); t.AddDate(0, 0, 1) {
+		days = append(days, t.Format("20060102"))
+	}
+	return
+}
+
+func inMonth(target, start, end time.Time) bool {
+	if start.After(target) && end.Before(target) {
+		return true
+	}
+	if target.Year() == start.Year() && target.Month() == start.Month() {
+		return true
+	}
+	if target.Year() == end.Year() && target.Month() == end.Month() {
+		return true
+	}
+	return false
 }
 
 func NewBucketService(r repo.IBudgetRepo) IBucketService {
--- src/service/service_test.go
+++ src/service/service_test.go
@@ -1,6 +1,10 @@
 package service
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+	"time"
+)
 
 func Test_daysIn(t *testing.T) {
 	type args struct {
@@ -11,11 +15,10 @@ func Test_daysIn(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
 		{
 			name: "",
 			args: args{
-				ym: "202206",
+				ym: "202202",
 			},
 			want: 28,
 		},
@@ -24,6 +27,34 @@ func Test_daysIn(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := MonthDays(tt.args.ym); got != tt.want {
 				t.Errorf("daysIn() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetAllDays(t *testing.T) {
+	type args struct {
+		start time.Time
+		end   time.Time
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantDays []string
+	}{
+		{
+			name:     "",
+			args:     args{
+				start: time.Time{},
+				end:   time.Time{},
+			},
+			wantDays: []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotDays := GetAllDays(tt.args.start, tt.args.end); !reflect.DeepEqual(gotDays, tt.wantDays) {
+				t.Errorf("GetAllDays() = %v, want %v", gotDays, tt.wantDays)
 			}
 		})
 	}
